f32256606e78a352c907111661df53987698d134
Udaya Sri <waddudaya2018@outlook.com>
2025-01-16 14:54:51 +0530

    register css ceate  2025/01/16 update 3

--- auth/register.php
+++ auth/register.php
@@ -59,7 +59,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Register</title>
-    <link rel="stylesheet" href="../assets/css/log_style.css"> <!-- Link to your CSS file -->
+    <link rel="stylesheet" href="../assets/css/register_style.css"> <!-- Link to your CSS file -->
 </head>
 <body>
     <div class="register-container">
